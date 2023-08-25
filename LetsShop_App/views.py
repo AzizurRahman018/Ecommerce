ea35ee08e782856812b5739ac7a15158865cdc5b
Md Azizur Rahman <106616550+Mitonaziz@users.noreply.github.com>
2023-08-25 20:21:37 +0600

    update databse

--- LetsShop_App/views.py
+++ LetsShop_App/views.py
@@ -3,7 +3,7 @@ from .models import *
 # Create your views here.
 def Home(request):
     slides= Slider.objects.all()
+    cats = Category.objects.all()
 
 
-
-    return render(request,'Home.html',{'slides':slides} )
+    return render(request,'Home.html',{'slides':slides, 'cats':cats} )
